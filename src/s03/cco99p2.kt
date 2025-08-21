31edd25eecdfaec8cabde294035acc71400785d8
kazakong <kazakong@naver.com>
2025-08-21 19:11:17 +0900

    s3

--- src/s03/cco99p2.kt
+++ src/s03/cco99p2.kt
@@ -1,46 +1,46 @@
 package s03
 
-package s03
-
 fun main() {
-    val n = readLine()!!.toInt()
 
-    for (i in 1..n) {
-        val input = readLine()!!.split(" ")
-        val m = input[0].toInt()
-        val k = input[1].toInt()
+    val testCaseCount = readLine()!!.toInt()
 
-        val counts = mutableMapOf<String, Int>()
+    for (test in 1..testCaseCount) {
+        val line = readLine()!!.split(" ")
+        val m = line[0].toInt()
+        val k = line[1].toInt()
 
-        var j = 0
-        while (j < m) {
+        val wordCounts = mutableMapOf<String, Int>()
+
+        for (i in 1..m) {
             val word = readLine()!!
-            if (counts.containsKey(word)) {
-                counts[word] = counts[word]!! + 1
+            if (wordCounts.containsKey(word)) {
+                wordCounts[word] = wordCounts[word]!! + 1
             } else {
-                counts[word] = 1
+                wordCounts[word] = 1
             }
-            j++
         }
 
-        val freqMap = mutableMapOf<Int, MutableList<String>>()
-        for ((word, count) in counts) {
-            if (!freqMap.containsKey(count)) {
-                freqMap[count] = mutableListOf()
+        val sortedList = wordCounts.entries
+            .sortedByDescending { it.value } // 값 기준 내림차순 정렬
+
+        val freqList = mutableListOf<Int>()
+        for (entry in sortedList) {
+            if (!freqList.contains(entry.value)) {
+                freqList.add(entry.value)
             }
-            freqMap[count]!!.add(word)
         }
 
-        val sortedFreq = freqMap.keys.sortedDescending()
+        println("${k}번째로 많이 나온 단어:")
 
-        println("${k}th most common word:")
-
-        if (k <= sortedFreq.size) {
-            val freq = sortedFreq[k - 1]
-            val words = freqMap[freq]!!
-            for (w in words) {
-                println(w)
+        if (k <= freqList.size) {
+            val targetCount = freqList[k - 1]
+            for ((word, count) in sortedList) {
+                if (count == targetCount) {
+                    println(word)
+                }
             }
+        } else {
+            println("해당 순위는 없습니다.")
         }
 
         println()
